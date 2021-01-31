8e8e19e3a43edcd8ffee053824d8a914b758cf69
Kugel <urmom@urmom.com>
2021-01-31 11:29:14 -0800

    initial Commit

---
+++ files/example.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    Token: "<Discord Token>",
+    Prefix: ">",
+    Role:"igibuattention",
+    Status:"with Kugel-kun's Computer's",
+    instanceIDS: ['i-0d628c2b3aa73fd72'],
+}
